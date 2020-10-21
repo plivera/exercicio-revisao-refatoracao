b699783cef5c2127735098c3dac916465c2ca7ad
plivera <73171545+plivera@users.noreply.github.com>
2020-10-20 23:41:27 -0300

    Update Empregado.hpp

--- Empregado.hpp
+++ Empregado.hpp
@@ -4,25 +4,14 @@
 #include <iostream>
 #include <string>
 
-class Empregado {
-	
-  public:
-    double salarioHora;  
-    double quotaMensalVendas;  
-
-
-    double pagamentoMes(double horasTrabalhadas) {
- 
-      double t = horasTrabalhadas;
-	  
-	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > 8) {
-        double x = horasTrabalhadas - 8;
-        t += x / 2;
-      }
-	  return t * salarioHora;
-    }
-	
-};
-
+namespace Funcionario {
+  class Empregado {
+    private:
+      static constexpr int HORARIO_TRABALHO = 8;//quantidade padrao de horas trabalhadas.
+    public:
+      double salarioHora;  
+      double quotaMensalVendas;
+      double pagamentoMes(double horasTrabalhadas);
+  };
+}
 #endif
